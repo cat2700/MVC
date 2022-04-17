d09b1648689e7c553c14a161eb9b8d62b3473468
Haitham Maged Elkot <cat2700@gmail.com>
2022-04-17 11:11:12 +0200

    e

---
+++ A-proj1/proj1/Models/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace proj1.Models
+{
+    public class Student
+    {
+        public int stID { get; set; }
+
+        public string stFName { get; set; }
+
+        public string stLName { get; set; }
+    }
+}
