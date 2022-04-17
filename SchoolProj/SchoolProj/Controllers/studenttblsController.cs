a245e6b5abbae831a45c75f82a81e19e14ebeec5
Haitham Maged Elkot <cat2700@gmail.com>
2022-04-17 19:50:12 +0200

    c

---
+++ SchoolProj/SchoolProj/Controllers/studenttblsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using SchoolProj.Models;
+
+namespace SchoolProj.Controllers
+{
+    public class studenttblsController : Controller
+    {
+        private schooldbEntities db = new schooldbEntities();
+
+        // GET: studenttbls
+        public ActionResult Index()
+        {
+            return View(db.studenttbls.ToList());
+        }
+
+        // GET: studenttbls/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            studenttbl studenttbl = db.studenttbls.Find(id);
+            if (studenttbl == null)
+            {
+                return HttpNotFound();
+            }
+            return View(studenttbl);
+        }
+
+        // GET: studenttbls/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: studenttbls/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,fname,lname,enrolldate")] studenttbl studenttbl)
+        {
+            if (ModelState.IsValid)
+            {
+                db.studenttbls.Add(studenttbl);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(studenttbl);
+        }
+
+        // GET: studenttbls/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            studenttbl studenttbl = db.studenttbls.Find(id);
+            if (studenttbl == null)
+            {
+                return HttpNotFound();
+            }
+            return View(studenttbl);
+        }
+
+        // POST: studenttbls/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,fname,lname,enrolldate")] studenttbl studenttbl)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(studenttbl).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(studenttbl);
+        }
+
+        // GET: studenttbls/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            studenttbl studenttbl = db.studenttbls.Find(id);
+            if (studenttbl == null)
+            {
+                return HttpNotFound();
+            }
+            return View(studenttbl);
+        }
+
+        // POST: studenttbls/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            studenttbl studenttbl = db.studenttbls.Find(id);
+            db.studenttbls.Remove(studenttbl);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ SchoolProj/SchoolProj/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SchoolProj.Startup))]
+namespace SchoolProj
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
