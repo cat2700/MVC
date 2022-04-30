e6b767de146f764a9c10a14acd70a1206fd99b51
Haitham Maged Elkot <cat2700@gmail.com>
2022-04-30 23:25:04 +0200

    bb

---
+++ LINQ-Console - Advanced/LINQ-Console/BookRepo.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ_Console
+{
+    class BookRepo
+    {
+        public List<Book> GetBooks()
+        {
+            return new List<Book>
+            {
+                new Book{title="ASP.NET MVC",price=30 ,category="Data Access",
+                    recommendations= new string[]{"DR, Haitham","DR. Mazen"}},
+                new Book{title="ASP.NET WEP API",price=17,category="Web Programing",
+                    recommendations= new string[]{"DR, maged","DR. Mazen"}},
+                new Book{title="ASP.NET step by step",price=4,category="Web Programing",
+                    recommendations= new string[]{"DR, Mohammed","DR. Ebrahim"}},
+                new Book{title="C# Advanced Topics",price=9,category="Programing",
+                    recommendations= new string[]{"DR, Mohammed","DR. Ebrahim"}},
+                new Book{title="Java Advanced Topics",price=22,category="Programing",
+                    recommendations= new string[]{"DR. Ebrahim"}},
+                new Book{title="Python OOP",price=4.5f,category="Programing",
+                    recommendations= new string[]{"DR, khadija","DR. Ebrahim"}},
+                new Book{title="Python Intro",price=2,category="Programing",
+                    recommendations= new string[]{}},
+
+            };
+        }
+
+
+        public List<Book> GetBooks2()
+        {
+            return new List<Book>
+            {
+                new Book{title="Entity Framework MVC Core",price=19 ,category="Data Access",
+                    recommendations= new string[]{"DR, ahmed"}},
+                new Book{title="Entity Framework WEP API",price=7,category="Web Programing",
+                    recommendations= new string[]{"DR, hamed","DR. Mazen"}},
+                new Book{title="ASP.NET step by step",price=4,category="Web Programing",
+                    recommendations= new string[]{"DR, Mohammed","DR. Ebrahim"}},
+                new Book{title="C# Advanced Topics",price=9,category="Programing",
+                    recommendations= new string[]{"DR, Mohammed","DR. Ebrahim"}},
+                new Book{title="Java Advanced Topics",price=22,category="Programing",
+                    recommendations= new string[]{"DR. Ebrahim"}},
+                new Book{title="Python OOP",price=4.5f,category="Programing",
+                    recommendations= new string[]{"DR, khadija","DR. Ebrahim"}},
+                new Book{title="Python Intro",price=2,category="Programing",
+                    recommendations= new string[]{}},
+
+            };
+        }
+    }
+}
---
+++ LINQ-Console - Advanced/LINQ-Console/Program.cs
@@ -0,0 +1,260 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //oldprocess();
+
+            //LINQ_Extention_Methos_Examples();
+
+            //LINQ_Query_Operator();
+
+            //LINQ_ReturnOneRow();
+
+            //LINQ_Pagination();
+
+            //LINQ_Aggregates();
+
+            //returnMoreColAndUsingAlias();
+
+            //returnColAsArray();
+
+            //returnWithMultiCondetions();
+
+            //returnGroupsBy();
+
+            //returnInnerJoin();
+
+            resultme();
+
+            Console.ReadKey();
+        }
+
+        private static void resultme()
+        {
+            // >> Union == Union All
+
+            // >> Get Data:
+            var books1 = new BookRepo().GetBooks();
+            var books2 = new BookRepo().GetBooks2();
+
+            var returndata = (from a in books1 select a).Union(from b in books2 select b).ToList();
+
+            // >> Display:
+            foreach (var c in returndata)
+            {
+                Console.WriteLine(c.title + " >> " + c.price);
+            }
+        }
+
+        private static void returnInnerJoin()
+        {
+            // >> join == inner join
+
+            // >> Get Data:
+            var books1 = new BookRepo().GetBooks();
+            var books2 = new BookRepo().GetBooks2();
+
+            var returndata = from a in books1
+                             join b in books2
+                             on a.title equals b.title
+                             select new { a.title ,a.price };
+
+            // >> Display:
+            foreach (var c in returndata)
+            {
+                Console.WriteLine(c.title + " >> " + c.price);
+            }
+        }
+
+        private static void returnGroupsBy()
+        {
+            // >> Get Data:
+            var books = new BookRepo().GetBooks();
+
+            var returndata = from b in books
+                             group b by b.category;
+
+            // >> Display:
+            foreach (var c in returndata)
+            {
+                Console.WriteLine(c.Key + " >> " + c.Count());
+            }
+        }
+
+        private static void returnWithMultiCondetions()
+        {
+            // >> Get Data:
+            var books = new BookRepo().GetBooks();
+
+            var returndata = from b in books
+                             where b.price > 4 && b.title.ToLower().StartsWith("a")
+                             select new { BookTitle = b.title, BookPrice = b.price };
+
+            // >> Display:
+            foreach (var c in returndata)
+            {
+                Console.WriteLine(c.BookTitle + " >> " + c.BookPrice);
+            }
+        }
+
+        private static void returnColAsArray()
+        {
+            // >> Get Data:
+            var books = new BookRepo().GetBooks();
+
+            var moreColWithAlias = from b in books
+                                   from r in b.recommendations
+                                   select new { BookTitle = b.title, BookPrice = b.price, recommend = r };
+
+
+            // >> Display:
+            foreach (var c in moreColWithAlias)
+            {
+                Console.WriteLine(c.BookTitle + " >> " + c.BookPrice + " >> " + c.recommend);
+            }
+        }
+
+        private static void returnMoreColAndUsingAlias()
+        {
+            // >> Get Data:
+            var books = new BookRepo().GetBooks();
+
+            var moreColWithAlias = from b in books
+                                   select new { BookTitle = b.title, BookPrice = b.price };
+
+
+            // >> Display:
+            foreach (var c in moreColWithAlias)
+            {
+                Console.WriteLine(c.BookTitle + " >> " + c.BookPrice);
+            }
+        }
+
+        private static void LINQ_Aggregates()
+        {
+            // >> count , max , min , sum
+
+            // >> Get Data:
+            var books = new BookRepo().GetBooks();
+
+            var count = books.Count();
+            var max = books.Max(v => v.price);
+            var min = books.Min(v => v.price);
+            var aver = books.Average(v => v.price);
+            var sum = books.Sum(v => v.price);
+
+            // >> Display:   
+            Console.WriteLine(count);
+            
+        }
+
+        private static void LINQ_Pagination()
+        {
+            // مهم جدا في عمل جريد به صفحات لعرض الحقول الكثيرة على اكثر من صفحة
+
+            // >> Get Data:
+            var books = new BookRepo().GetBooks();
+
+            var selectedBooks = books.Skip(2).Take(2);
+
+            // >> Display:
+            foreach (var c in selectedBooks)
+            {
+                Console.WriteLine(c.title + " >> " + c.price);
+            }
+
+        }
+
+        private static void LINQ_ReturnOneRow()
+        {
+            // >> Get Data:
+            var books = new BookRepo().GetBooks();
+
+            // >> LINQ return one row:
+            //var oneRow = books.Single(x => x.title == "Python OOP "); // will exception if return null or more one row
+            //var oneRow = books.SingleOrDefault(x => x.price > 4); //// will exception if return more one row
+            //var oneRow = books.FirstOrDefault(x => x.price > 4);
+            var oneRow = books.LastOrDefault(x => x.price > 4);
+
+            // >> Display:
+            if (oneRow != null)
+            {
+                Console.WriteLine(oneRow.title + " " + oneRow.price);
+            }
+        }
+
+        private static void LINQ_Query_Operator()
+        {
+            // >> Get Data:
+            var books = new BookRepo().GetBooks();
+
+            // >> LINQ Query Operator:
+            var cheapBooks = from xx in books
+                             where xx.price < 10
+                             orderby xx.price descending
+                             select xx.title; // >> projection
+            // >> Display:
+            foreach (var c in cheapBooks)
+            {
+                Console.WriteLine(c);
+            }
+
+
+
+        }
+
+        private static void LINQ_Extention_Methos_Examples()
+        {
+            // >> Get Data:
+            var books = new BookRepo().GetBooks();
+
+            var cheapbooks = books.Where(bk => bk.price < 10).OrderByDescending(x => x.price);
+
+            // >> Display:
+            foreach (var c in cheapbooks)
+            {
+                Console.WriteLine(c.title + " >> " + c.price);
+            }
+
+            Console.ReadKey();
+
+
+            //use LINQ to select one column as :
+            var cheapbooksTitle = books.Where(bk => bk.price < 10).OrderByDescending(x => x.price).Select(bk => bk.title);
+
+            // >> Display as string:
+            foreach (var t in cheapbooksTitle)
+            {
+                Console.WriteLine(t);
+            }
+
+        }
+
+        private static void oldprocess()
+        {
+            var books = new BookRepo().GetBooks();
+
+            // >> General Process:
+            var cheapbooks = new List<Book>();
+
+            foreach (var book in books)
+            {
+                if (book.price < 12)
+                {
+                    cheapbooks.Add(book);
+                }
+            }
+
+        }
+
+
+    }
+}
